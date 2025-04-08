f1bfe0beacc632567ef58c09617e6223d54e619a
derekgreene11 <derek@derekrgreene.com>
2025-04-07 20:31:52 -0700

    Initial commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+   {
+   	"nvim-treesitter/nvim-treesitter",
+   	opts = {
+   		ensure_installed = {
+   			"vim", "lua", "vimdoc",
+        "html", "css"
+   		},
+   	},
+   },
+{
+    "kdheepak/lazygit.nvim",
+    lazy = true,  -- Load lazily
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = { "nvim-lua/plenary.nvim" },  -- LazyGit depends on plenary.nvim
+    keys = { { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" } },  -- Keybinding for LazyGit (Space + gg)
+  },
+}
