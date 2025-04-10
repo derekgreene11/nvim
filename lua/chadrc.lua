ba500ace9cc409258c3d9fc56187a739db6cd6ae
derekgreene11 <derekgreene11@gmail.com>
2025-04-09 22:35:02 -0700

    added transparency option

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,8 +6,8 @@
 local M = {}
 
 M.base46 = {
-	theme = "eldritch",
-
+	theme = "bearded-arc",
+  --transparency = true,
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
